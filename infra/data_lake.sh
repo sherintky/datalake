1599a52e3c6020707cceee0b05d90416d66b8cd6
sherintky <sherintky@rediffmail.com>
2020-02-29 22:29:31 +0530

    datalake 1.1

--- infra/data_lake.sh
+++ infra/data_lake.sh
@@ -1,3 +1,6 @@
+
+###Resource Group###
+
 #!/bin/bash
 DATA_LAKE_RG='datalake-rg'
 DATA_LAKE_LOCATION='eastus'
@@ -6,6 +9,7 @@ az group create \
 --name $DATA_LAKE_RG \
 --location $DATA_LAKE_LOCATION
 
+###Data lake Storage###
 
 DATA_LAKE_STORAGE='datalakest'
 # Create a new storage (ADLS Gen2) Account
@@ -17,6 +21,7 @@ az storage account create \
 --kind StorageV2  \
 --hierarchical-namespace true
 
+###Data Factory###
 DATA_LAKE_WORKFLOW='datalakeorchestrator'
 ARM_LOCATION='arm/data_factory.json'
 ARM_PROPS_LOCATION='../conf/data_factory_prop.json'
@@ -26,3 +31,32 @@ az group deployment create \
  --resource-group $DATA_LAKE_RG \
  --template-file $ARM_LOCATION \
  --parameters $ARM_PROPS_LOCATION
+
+
+###Database###
+DATA_LAKE_SERVER_NAME='datalakesr'
+DATA_LAKE_DATABASE_NAME='datalakedb'
+ADMIN_USERNAME='dbuser'
+ADMIN_PASSWD='Mindtree_123'
+## Create a logical server in the resource group
+az sql server create \
+ --name $DATA_LAKE_SERVER_NAME \
+ --resource-group $DATA_LAKE_RG \
+ --location $DATA_LAKE_LOCATION  \
+ --admin-user $ADMIN_USERNAME \
+ --admin-password $ADMIN_PASSWD
+## Create a database in the server 
+az sql db create \
+ --resource-group $DATA_LAKE_RG \
+ --server $DATA_LAKE_SERVER_NAME \
+ --name $DATA_LAKE_DATABASE_NAME \
+ --sample-name AdventureWorksLT \
+ --service-objective S0
+# Configure a firewall rule for the server
+az sql server firewall-rule create \
+ --resource-group $DATA_LAKE_RG \
+ --server $DATA_LAKE_SERVER_NAME \
+ -n AllowYourIp \
+ --start-ip-address 0.0.0.0 \
+ --end-ip-address 0.0.0.0
+
